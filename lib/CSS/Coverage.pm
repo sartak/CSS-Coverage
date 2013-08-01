261ef224182b047d061221f83337d669a11d7d36
Shawn M Moore <code@sartak.org>
2013-08-01 13:46:39 -0400

    CSS::Coverage->check

--- lib/CSS/Coverage.pm
+++ lib/CSS/Coverage.pm
@@ -1,6 +1,19 @@
 package CSS::Coverage;
 use Moose;
+use CSS::SAC;
 use CSS::Coverage::Deparse;
+
+has css_file => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has html_files => (
+    is       => 'ro',
+    isa      => 'ArrayRef[Str]',
+    required => 1,
+);
 
 has deparser => (
     is      => 'bare',
@@ -25,6 +38,16 @@ sub end_selector {
     }
 
     print "\n";
+}
+
+sub check {
+    my $self = shift;
+
+    my $sac = CSS::SAC->new({
+        DocumentHandler => $self,
+    });
+
+    $sac->parse({ filename => $self->css_file });
 }
 
 __PACKAGE__->meta->make_immutable;
