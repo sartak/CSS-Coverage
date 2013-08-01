cebb8498ad016c54a03feff6c0fcdc8a61cfdbc3
Shawn M Moore <code@sartak.org>
2013-08-01 16:05:15 -0400

    Report doc

--- lib/CSS/Coverage/Report.pm
+++ lib/CSS/Coverage/Report.pm
@@ -13,3 +13,17 @@ has unmatched_selectors => (
 
 1;
 
+__END__
+
+=head1 NAME
+
+CSS::Coverage::Report
+
+=head1 METHODS
+
+=head2 unmatched_selectors
+
+Returns a list of CSS selectors that were not matched against any document.
+
+=cut
+
