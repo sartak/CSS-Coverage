211bf096edb36e43a2434dab16b5380cacfce73d
Shawn M Moore <code@sartak.org>
2013-08-01 14:21:20 -0400

    Handle :hover

--- lib/CSS/Coverage/XPath.pm
+++ lib/CSS/Coverage/XPath.pm
@@ -3,5 +3,15 @@ use strict;
 use warnings;
 use base 'HTML::Selector::XPath';
 
+sub parse_pseudo {
+    my ($self, $pseudo) = @_;
+
+    if ($pseudo eq 'hover') {
+        return "[true()]";
+    }
+
+    return;
+}
+
 1;
 
