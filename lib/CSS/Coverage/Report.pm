d75d490a6b275e9963f5a9769202dc2dc1889393
Shawn M Moore <code@sartak.org>
2013-08-01 15:29:18 -0400

    default of []

--- lib/CSS/Coverage/Report.pm
+++ lib/CSS/Coverage/Report.pm
@@ -2,8 +2,9 @@ package CSS::Coverage::Report;
 use Moose;
 
 has unmatched_selectors => (
-    traits => ['Array'],
-    is     => 'bare',
+    traits  => ['Array'],
+    is      => 'bare',
+    default => sub { [] },
     handles => {
         unmatched_selectors    => 'elements',
         add_unmatched_selector => 'push',
